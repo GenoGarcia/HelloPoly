eed10f676d7286c94f6da07c5ad657e34507ec95
CISSAPP-13M <cissapp-13m@CISSAPP-13Ms-MacBook-Pro.local>
2015-05-17 23:41:24 -0600

    Fixes
    Runs, but not properly…

--- HelloPoly/ViewController.h
+++ HelloPoly/ViewController.h
@@ -9,15 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+
 @property (strong, nonatomic) IBOutlet UIStepper *stepper;
+
 @property (strong, nonatomic) IBOutlet UILabel *result;
 - (IBAction)stepper:(UIStepper *)sender;
+
 @property (weak, nonatomic) IBOutlet UILabel *label;
-- (IBAction)buttonPressed:(id)sender
-{
-    NSString *title = [sender titleForState:UIControlStateNormal];
-    NSString *plainText = [NSString stringWithFormat:@"%@ button pressed.", title];
-    _statusLabel.text = plainText;
-}
+- (IBAction)buttonPressed:(id)sender;
 @end
 
